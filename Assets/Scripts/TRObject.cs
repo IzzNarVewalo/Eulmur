e03eb7d9597c39f17de5b6ea27a3b49eb08e0e3b
friedliche <irina.tsareva@hotmail.com>
2016-01-23 16:13:22 +0100

    Interface, abstrakte Klasse hinzugefugt, mache nun noch dass ich sie an die Spieler ranhaenge

---
+++ Assets/Scripts/TRObject.cs
@@ -0,0 +1,7 @@
+﻿interface ITR
+{
+	void SaveTRObject();
+	void LoadTRObject(TRObject trobject);
+}
+
+abstract public class TRObject{}
---
+++ Assets/Scripts/TimeReverse.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class TimeReverse : MonoBehaviour {
+
+	private Stack<TRObject> objectsOnStack = new Stack<TRObject>();
+
+	private ITR otherScript;
+	void Start ()
+	{
+		otherScript= (ITR)gameObject.GetComponent (typeof(ITR));
+	}
+
+	void FixedUpdate ()
+	{
+
+		if
+			(Input.GetButton ("TimeControl"))
+		{
+			if(objectsOnStack.Count > 0)
+				otherScript.LoadTRObject (objectsOnStack.Pop());
+		}
+		else
+
+		otherScript.SaveTRObject (); //um frameunabhaengig zu bleiben
+	}
+}
