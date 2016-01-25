0058bfcca9364c0ffb1b02704b9193cd71adbdd8
friedliche <irina.tsareva@hotmail.com>
2016-01-25 21:21:01 +0100

    ein Affe

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,14 +6,32 @@ public class Enemy : MonoBehaviour {
 	public bool invincible;
 	public float bumpSpeed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+
+	public Variablen variablen;
+
+
+
 	// Update is called once per frame
 	void Update () {
-	
+
+
+
+		float amtToMove = variablen.currentSpeed * Time.deltaTime; 
+
+		//             bewege dich
+		transform.Translate (Vector2.right * amtToMove, Space.World);
+
+
+	}
+
+
+	//nur wenn trigger berührt
+	void OnTriggerEnter2D(Collider2D other) //nur prüfen, welches Object berührt <-> collision: nicht nur welches, sondern auch wo wie genau
+	{		
+		if(other.tag == "Grenze"){
+
+			variablen.currentSpeed *= -1; 
+		}
 	}
 
 	public void OnDeath()
