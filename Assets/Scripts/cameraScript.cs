2fea6afc01e8094ab5b00d043e7c59ef1198470f
IzzNarVewalo <cyprian.manthey@cs.tum.edu>
2016-01-25 01:19:20 +0100

    PlayerBoundings

--- Assets/Scripts/cameraScript.cs
+++ Assets/Scripts/cameraScript.cs
@@ -6,23 +6,20 @@ public class cameraScript : MonoBehaviour
     public GameObject Owl, Lemur;
 
     // Update is called once per frame
-    /*void Update()
-    {
-        float blah = (Owl.transform.position.x + Lemur.transform.position.x) / 2 - transform.position.x -2f;
-        Vector3 whereToGo = new Vector3(blah, 0, 0);
-        transform.Translate(whereToGo, Space.World);
-    }*/
-
-
-        void OnTriggerEnter2D(Collider2D other)
-    {
-
-    }
-
-    void LateUpdate()
+    void Update()
     {
         float blah = (Owl.transform.position.x + Lemur.transform.position.x) / 2 - transform.position.x - 2f;
         Vector3 whereToGo = new Vector3(blah, 0, 0);
         transform.Translate(whereToGo, Space.World);
+    }
+    
+    void LateUpdate()
+    {
+
+
+        if (transform.position.x <= -1.5f)
+            transform.position = new Vector3(-1.5f, transform.position.y, transform.position.z);
+
+
     }
 }
