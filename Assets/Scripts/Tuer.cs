cc85dc18fc5b948917b7038f4ac64b6aef99af7c
friedliche <irina.tsareva@hotmail.com>
2016-01-27 16:29:44 +0100

    lol

---
+++ Assets/Scripts/Tuer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tuer : MonoBehaviour {
+
+
+	void OnCollisionEnter2D(Collision2D other){
+
+
+		Application.LoadLevel ("Endszene");
+
+	}
+}
