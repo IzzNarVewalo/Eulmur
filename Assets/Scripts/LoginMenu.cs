5ed7c9fd5f9f9cd2ec9017b5824596287fbf59c9
IzzNarVewalo <cyprian.manthey@cs.tum.edu>
2016-01-27 18:17:16 +0100

    save

---
+++ Assets/Scripts/LoginMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoginMenu : MonoBehaviour {
+
+    public void createAccount()
+    {
+        Application.OpenURL("http://www.1000sunny.de/");
+    }
+
+    public void playOffline()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void playOnline()
+    {
+
+    }
+}
