d85754c43bc0ac8eaf023e01ede5dc1b3130016c
IzzNarVewalo <cyprian.manthey@cs.tum.edu>
2016-01-28 01:09:12 +0100

    fixed DB

--- Assets/Scripts/LoginMenu.cs
+++ Assets/Scripts/LoginMenu.cs
@@ -8,12 +8,13 @@ public class LoginMenu : MonoBehaviour {
 
     public void createAccount()
     {
-        Application.OpenURL("http://www.1000sunny.de/games/eulmur/create");
+        SceneManager.LoadScene(1);
+        //Application.OpenURL("http://www.1000sunny.de/games/eulmur/create");
     }
 
     public void playOffline()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
 
     public void playOnline()
@@ -25,18 +26,47 @@ public class LoginMenu : MonoBehaviour {
     {
         var form = new WWWForm();
         form.AddField("benutzername",GameObject.FindGameObjectWithTag("Username").GetComponent<InputField>().text);
-        form.AddField("passwort", Md5Sum(GameObject.FindGameObjectWithTag("Password").GetComponent<InputField>().text+"$K?1/S_@2%e#el!3>s#5BRo$a1+"));
+        form.AddField("passwort", md5Sum(GameObject.FindGameObjectWithTag("Password").GetComponent<InputField>().text+"$K?1/S_@2%e#el!3>s#5BRo$a1+"));
         var connection = new WWW("http://www.1000sunny.de/games/eulmur/login.php", form);
         yield return connection;
         if (connection.error != null)
-            Debug.Log(connection.error);
-        else if (connection.text.Equals(1))
-            Debug.Log("test");
+            Debug.Log("ouldn't connect to the server: "+connection.error );
+        else if (connection.text.Equals("\n1"))
+            Debug.Log("yes");
+            //get WWWData from DB
         else
-            Debug.Log(connection.text);
+            Debug.Log("nö"+ connection.text);
     } 
 
-    public string Md5Sum(string strToEncrypt)
+    public void backToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void newAccount()
+    {
+        if (GameObject.FindGameObjectWithTag("Username").GetComponent<InputField>().text.Equals("") || GameObject.FindGameObjectWithTag("Password").GetComponent<InputField>().text.Equals(""))
+            Debug.Log("passwort oder benutzername darf nicht leer sein");
+        else if (GameObject.FindGameObjectWithTag("Password").GetComponent<InputField>().text.Equals(GameObject.FindGameObjectWithTag("RepeatPassword").GetComponent<InputField>().text))
+            StartCoroutine(insertToDB());
+        else
+            Debug.Log("PW 2 mal unterschiedlich");
+    }
+
+    IEnumerator insertToDB()
+    {
+        var form = new WWWForm();
+        form.AddField("benutzername", GameObject.FindGameObjectWithTag("Username").GetComponent<InputField>().text);
+        form.AddField("passwort", md5Sum(GameObject.FindGameObjectWithTag("Password").GetComponent<InputField>().text + "$K?1/S_@2%e#el!3>s#5BRo$a1+"));
+        var connection = new WWW("http://www.1000sunny.de/games/eulmur/create.php", form);
+        yield return connection;
+        if (connection.error != null)
+            Debug.Log(connection.error);
+        else
+            Debug.Log("Account erstellt");
+    }
+
+    public string md5Sum(string strToEncrypt)
     {
         var ue = new System.Text.UTF8Encoding();
         var bytes = ue.GetBytes(strToEncrypt);
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerData : MonoBehaviour {
+
+    public static string username;
+    public static int hp = 5;
+    public static int money = 0;
+    public static int food = 0;
+
+    public static bool moustache = false;
+    public static bool unicornhorn = false;
+    public static bool crown = false;
+
+    // Update is called once per frame
+    void LoadData (string username, int hp, int money, int food, bool moustache, bool unicornhorn, bool crown) {
+        PlayerData.username = username;
+        PlayerData.hp = hp;
+        PlayerData.money = money;
+        PlayerData.food = food;
+        PlayerData.moustache = moustache;
+        PlayerData.unicornhorn = unicornhorn;
+        PlayerData.crown = crown;
+	}
+}
