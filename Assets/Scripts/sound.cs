17b83d9a87df08a16097eb8ab3c7cb6257bc1490
IzzNarVewalo <cyprian.manthey@cs.tum.edu>
2016-02-02 21:15:07 +0100

    demoday

---
+++ Assets/Scripts/sound.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sound : MonoBehaviour {
+
+    public AudioSource Wald;
+    public AudioSource Wasserfall;
+    public AudioSource FallendeAffen;
+
+    // Use this for initialization
+    void OnTriggerEnter (Collider other) {
+	    
+	}
+}
